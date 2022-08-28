6f1c0ddfad191e1bf249d741d5b793c5a9ff6963
MikeMutek <michael.mutek@gmail.com>
2022-08-28 01:20:17 -0400

    First Commit

---
+++ RedRoverCodePuzzle/InitialStringHelper.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RedRoverCodePuzzle
+{
+    internal static class InitialStringHelper
+    {
+        //(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
+
+
+        //- id
+        //- name
+        //- email
+        //- type
+        //  - id
+        //  - name
+        //  - customFields
+        //      - c1
+        //      - c2
+        //      - c3
+        //- externalId
+
+
+
+
+        private static readonly Dictionary<char, char> _parenthesisPairs = new()
+        {
+            { '(', ')' },
+        };
+
+
+
+        /// <summary>
+        /// Created On 8/28/2022
+        /// Description: Prints to console as it reads the string.  Then, parses the string into a dictionary and prints to console alphabetical
+        /// Could have parsed the string into a standard dictionary, but decided to switch things up and parse it a different way.  Because?  Why not!
+        /// </summary>
+        /// <param name="puzzleString"></param>
+        public static void PrintAsItReads(string puzzleString)
+        {
+            if (string.IsNullOrEmpty(puzzleString))
+            {
+                Console.WriteLine("nice try");
+            }
+
+            List<string> tabs = new();
+
+            StringBuilder stringBuilder = new();
+            puzzleString = puzzleString.Replace(" ", "");
+
+            for (int i = 0; i < puzzleString.Length; i++)
+            {
+                char thisChar = puzzleString[i];
+
+                if (_parenthesisPairs.ContainsKey(thisChar) || thisChar == ',')
+                {
+                    if (_parenthesisPairs.ContainsKey(thisChar) && i > 0)
+                    {
+                        tabs.Add("\t");
+                    }
+
+                    stringBuilder.Append('\n');
+                    stringBuilder.Append(string.Join("", tabs));
+                    stringBuilder.Append("- ");
+                    continue;
+                }
+
+                if (_parenthesisPairs.Values.Contains(thisChar) && tabs.Count > 0)
+                {
+                    tabs.RemoveAt(0);
+                    continue;
+                }
+
+                stringBuilder.Append(thisChar);
+            }
+            stringBuilder.Append('\n');
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+
+
+
+        /// <summary>
+        /// Created On 8/28/2022
+        /// Description: Builds a SortedDictionary from the string and then print the alphabetically sorted keys
+        /// </summary>
+        /// <param name="puzzleString"></param>
+        public static void PrintAlphabetical(string puzzleString)
+        {
+            puzzleString = puzzleString.Replace(" ", "");
+
+            int stepCount = 0;
+            SortedDictionary<string, object> parsedPuzzle = ConvertStringToDictionary(puzzleString, 0, out stepCount);
+
+            WriteDictionaryContents(parsedPuzzle, string.Empty);
+
+        }
+
+        /// <summary>
+        /// Created By: MMutek
+        /// Created On 8/28/2022
+        /// Description: Recursive method to tease apart the string into a dictionary of string and object
+        /// </summary>
+        /// <param name="puzzleString">Completed string, expects RedRover puzzle format</param>
+        /// <param name="startIndex">Recursive parameter, used to keep track of where we are in the string</param>
+        /// <param name="stepsTaken">Recursive parameter, returns number of iterations made before returning</param>
+        /// <returns></returns>
+        private static SortedDictionary<string, object> ConvertStringToDictionary(string puzzleString, int startIndex, out int stepsTaken)
+        {
+            string thisKey = string.Empty;
+
+            SortedDictionary<string, object> subDictionary = new();
+
+            SortedDictionary<string, object> result = new SortedDictionary<string, object>();
+
+            stepsTaken = 0;
+
+            for (int i = startIndex; i < puzzleString.Length; i++)
+            {
+                char thisChar = puzzleString[i];
+                
+                if(string.IsNullOrEmpty(thisKey) && _parenthesisPairs.ContainsKey(thisChar))
+                {
+                    continue;
+                }
+
+                stepsTaken++;
+
+                if (thisChar == ',')
+                {
+                    result.Add(thisKey, subDictionary);
+                    thisKey = String.Empty;
+                    subDictionary = new SortedDictionary<string, object>();
+
+                    continue;
+                }
+
+                else if (_parenthesisPairs.ContainsKey(thisChar))
+                {
+                    int outIndex = 0;
+                    subDictionary = ConvertStringToDictionary(puzzleString, i, out outIndex);
+
+                    stepsTaken = stepsTaken + outIndex;
+                    i = i + outIndex;
+
+                    continue;
+                }
+
+                else if (_parenthesisPairs.Values.Contains(thisChar))
+                {
+                    result.Add(thisKey, subDictionary);
+                    thisKey = String.Empty;
+                    subDictionary = new SortedDictionary<string, object>();
+                    break;
+                }
+
+                else
+                {
+                    thisKey = $"{thisKey}{thisChar}";
+                }
+            }
+
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Created By: MMutek
+        /// Created On 8/28/2022
+        /// Description: Recursive method to iterate through the dictionary and writes all key's to console
+        /// </summary>
+        /// <param name="parsedPuzzle"></param>
+        /// <param name="indent">Formatting parameter used to keep track how far to indent</param>
+        private static void WriteDictionaryContents (SortedDictionary<string, object> parsedPuzzle, string indent)
+        {
+            Console.WriteLine();
+            foreach(var item in parsedPuzzle)
+            {
+                Console.WriteLine($"{indent}- {item.Key}");
+
+                SortedDictionary<string, object> itemValue = (SortedDictionary<string, object>)item.Value;
+                if(itemValue.Count > 0)
+                {
+                    string updatedIndent = $"{indent}\t";
+                    WriteDictionaryContents(itemValue, updatedIndent);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Created By: MMutek
+        /// Created On 8/28/2022
+        /// Description: validates input is properly formatted
+        /// </summary>
+        /// <param name="puzzleString"></param>
+        /// <returns></returns>
+        public static bool IsValidFormat(string puzzleString)
+        {
+            //no data, then sure, it counts
+            if (puzzleString.Count() == 0)
+            {
+                return true;
+            }
+
+            Stack<char> parenthesisStack = new();
+
+            foreach(char character in puzzleString)
+            {
+                if (_parenthesisPairs.ContainsKey(character))
+                {
+                    parenthesisStack.Push(character);
+                }
+                else if (_parenthesisPairs.Values.Contains(character))
+                {
+                    if (parenthesisStack.Count() == 0)
+                    {
+                        return false;
+                    }
+
+                    char openingParenthesis = parenthesisStack.Pop();
+                    //If it is not the last opening parenthesis, it fails validation
+                    if (_parenthesisPairs[openingParenthesis] != character)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return parenthesisStack.Count() == 0;
+        }
+
+
+    }
+}
---
+++ RedRoverCodePuzzle/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RedRoverCodePuzzle;
+
+string puzzleString = @"(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
+
+
+if (!InitialStringHelper.IsValidFormat(puzzleString))
+{
+    Console.WriteLine("Input not properly formatted");
+    Console.WriteLine($"\"{puzzleString}\" one or more parenthesis is missing");
+    Console.ReadKey();
+    Environment.Exit(Environment.ExitCode);
+}
+
+
+Console.WriteLine("Validation succeeded, moving on...");
+Console.WriteLine("First, we'll read and print it the string as first in, first out");
+Console.WriteLine("Any key to continue");
+Console.ReadKey();
+
+
+InitialStringHelper.PrintAsItReads(puzzleString);
+
+Console.WriteLine("Then, we'll parse the string into a SortedDictionary and print it alphabetically");
+Console.WriteLine("Any key to continue");
+Console.ReadKey();
+
+InitialStringHelper.PrintAlphabetical(puzzleString);
+
+
+Console.WriteLine("Thanks, this was fun (took me longer than an hour though!).  Ran into a bit of analysis paralysis...ugh - lol");
+Console.WriteLine();
+Console.WriteLine("Here's to hoping my solution wasn't too mundane!");
+
+Console.WriteLine("Any key to close");
+Console.ReadKey();
+Environment.Exit(Environment.ExitCode);
